d0e47c19984142481d7f6b1416c51a18c036ced1
Covali Andrei <krab4ek@gmail.com>
2022-07-13 21:40:43 +0300

    Add project files.

---
+++ covali.ICloneableExample/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("***** A First Look at Interfaces******\n");
+
+string mysStr = "Hello";
+OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+//System.Data.SqlTypes.SqlXml ex = new System.Data.SqlTypes.SqlXml();
+CloneMe(mysStr);
+CloneMe(unixOS);
+//CloneMe(ex);
+
+static void CloneMe(ICloneable c)
+{
+    object theClone = c.Clone();
+    Console.WriteLine($"Your clone is a: {theClone.GetType().Name}");
+}
